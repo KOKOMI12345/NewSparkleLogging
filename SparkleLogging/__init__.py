efbf37e76409878e4fb6cb4e6d9057d987c3c407
花火 <3072252442@qq.com>
2024-05-22 18:00:51 +0800

    Delete SparkleLogging directory

--- SparkleLogging/__init__.py
+++
@@ -1,8 +0,0 @@
-from SparkleLogging.core._manager import LogManager
-from SparkleLogging.core._level import Levels
-
-logger = LogManager.GetLogger("test",Levels.ON)
-
-"""
-只需要 from SparkleLogging import logger 就可以开始你的日志记录!
-"""
--- SparkleLogging/utils/_types.py
+++
@@ -1,6 +0,0 @@
-from SparkleLogging.dependencies import TypeVar
-
-
-Level = TypeVar("Level", int,int)
-AnyStr = TypeVar("AnyStr", str, bytes,object)
-Stream = TypeVar("Stream", str , bytes,object)
