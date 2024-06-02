fb24f61a11bc1e246e38a5165aee2e503e304f0e
花火 <3072252442@qq.com>
2024-06-02 13:03:56 +0800

    remove this 1.0.4 version and add version 1.0.5

--- NsparkleLog/_env.py
+++
@@ -1,4 +0,0 @@
-from NsparkleLog.utils._get_lock import get_current_lock
-default_format = "{localtime}.{msec:03d} | {level:<7} | {threadName} | {name}.{funcName} | {filename}:{lineno} - {message}"
-
-current_lock = get_current_lock()
