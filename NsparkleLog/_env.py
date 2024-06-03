cc88a664c586129e82af6497f58f67195343a769
花火 <3072252442@qq.com>
2024-06-03 23:22:50 +0800

    Update _env.py

--- NsparkleLog/_env.py
+++ NsparkleLog/_env.py
@@ -1,4 +1,4 @@
 import threading , multiprocessing
-default_format = "{localtime}.{msec:03d} | {level:<7} | {threadName} | {name}.{funcName} | {filename}:{lineno} - {message}"
+default_format = "{localtime}.{msec:03d} | {level:<7} | {ProcessName}.{threadName} | {name}.{funcName} | {filename}:{lineno} - {message}"
 
 allowed_lock_type = type(threading.Lock()) , type(multiprocessing.Lock())
