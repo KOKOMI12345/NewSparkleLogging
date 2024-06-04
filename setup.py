547b6b5279d10fd617e1a21315363844335a13a1
花火 <3072252442@qq.com>
2024-06-04 21:58:35 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 from setuptools import setup, find_packages
-from NsparkleLog import __version__
+from NsparkleLog import __version__ , __author__ , __packageName__
+
+def load_requirements(filepath: str = "requirements.txt") -> list[str]:
+    try:
+       with open(filepath, 'r') as f:
+           return [line.strip() for line in f.readlines()]
+    except FileNotFoundError:
+        return []
+    except Exception as e:
+        return []
 
 setup(
-    name='NsparkleLog',
+    name=__packageName__,
     version=__version__,
     packages=find_packages(),
-    author='花火official',
+    author=__author__,
     author_email='3072252442@qq.com',
-    description='A logging library for Python',
+    description='A useful logging library for Python',
     long_description=open('Readme.md','r',encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/KOKOMI12345/NewSparkleLogging',
+    requires=load_requirements(),  # 依赖
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
